6734299fa6f0c55e8dcbccdbecb093a5f3f4b728
ta3ls <realta3ls@gmail.com>
2026-08-19 22:46:04 +0800

    upgrade to Next.js 16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
